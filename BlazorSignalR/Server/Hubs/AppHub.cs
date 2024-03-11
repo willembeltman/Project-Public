bca387ce4b008a0dd0eff1bbfd443db5a7b175ee
ik willem <hwj.beltman@gmail.com>
2024-03-11 21:19:35 +0100

    BlazorSignalR

---
+++ BlazorSignalR/Server/Hubs/AppHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorSignalR.Server.Hubs
+{
+    public class AppHub : Hub
+    {
+        public async Task SendMessage(string user, string text)
+        {
+            await ReceiveMessage(user, text);
+        }
+        public async Task ReceiveMessage(string user, string text)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, text);
+        }
+    }
+}
