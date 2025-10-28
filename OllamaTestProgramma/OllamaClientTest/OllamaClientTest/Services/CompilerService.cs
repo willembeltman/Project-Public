570d2b575bc5797fb62e5226010a8b1e49d9288b
Willem Beltman <hwj.beltman@gmail.com>
2025-10-28 02:12:22 +0100

    a

--- OllamaTestProgramma/OllamaClientTest/OllamaClientTest/Services/CompilerService.cs
+++ OllamaTestProgramma/OllamaClientTest/OllamaClientTest/Services/CompilerService.cs
@@ -1,6 +1,6 @@
 ﻿namespace OllamaAgentGenerator.Services;
 
-internal class CompilerService
+public class CompilerService
 {
     private DirectoryInfo currentDirectory;
 
@@ -9,8 +9,15 @@ internal class CompilerService
         this.currentDirectory = currentDirectory;
     }
 
-    internal string Compile()
+    /// <summary>
+    /// Compiles anything in the currentDirectory
+    /// So, tries to find a .sln or .csproj file (.sln has priority) to compile
+    /// </summary>
+    /// <returns>Compilation status (example: "No .sln or .csproj file was found" or "Build succesfull")</returns>
+    public string Compile()
     {
+        // Find file to compile
+        // Compile it with process
         throw new NotImplementedException();
     }
 }
