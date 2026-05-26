d8873059e677d58d10117933947e5dc04a118194
Willem Beltman <hwj.beltman@gmail.com>
2026-05-26 05:11:34 +0200

    Srt ding

---
+++ SrtDing/SrtDing/Program.cs
@@ -0,0 +1,65 @@
+﻿var subtitles = SrtParser.Parse("OpnameEnverOrigineel.srt");
+
+var list = new List<ParsedSubtitle>();
+var currentIndex = 1;
+var currentSubtitle = (ParsedSubtitle?)null;
+var currentText = string.Empty;
+
+foreach (var subtitle in subtitles)
+{
+    var start = subtitle.Start;
+    var end = subtitle.End;
+    var speaker = subtitle.Text.Split(':').First();
+    var text = subtitle.Text.Substring(speaker.Length + 1).TrimStart();
+    if (currentSubtitle == null || 
+        currentSubtitle.Speaker != speaker ||
+        currentSubtitle.End != start ||
+        currentSubtitle.Text.Split('\n').Length > 7)
+    {
+        currentSubtitle = new ParsedSubtitle(currentIndex, start, end, speaker, text);
+        list.Add(currentSubtitle);
+        currentIndex++;
+    }
+    else
+    {
+        currentSubtitle.End = end;
+        currentSubtitle.Text += Environment.NewLine + text;
+    }
+}
+
+var output = string.Empty;
+
+foreach (var subtitle in list)
+{
+    output += $@"{subtitle.Index}
+{subtitle.Start:hh\:mm\:ss\,fff} --> {subtitle.End:hh\:mm\:ss\,fff}
+{subtitle.Speaker}: „{subtitle.Text}”
+
+";
+
+    Console.WriteLine($@"{subtitle.Index}
+{subtitle.Start:hh\:mm\:ss\,fff} --> {subtitle.End:hh\:mm\:ss\,fff}
+{subtitle.Speaker}: „{subtitle.Text}”
+
+");
+}
+
+File.WriteAllText("OpnameEnver.srt", output);
+
+class ParsedSubtitle
+{
+    public ParsedSubtitle(int index, TimeSpan start, TimeSpan end, string author, string text)
+    {
+        Index = index;
+        Start = start;
+        End = end;
+        Speaker = author;
+        Text = text;
+    }
+
+    public int Index { get; set; }
+    public TimeSpan Start { get; set; }
+    public TimeSpan End { get; set; }
+    public string Speaker { get; set; }
+    public string Text { get; set; }
+}
---
+++ SrtDing/SrtDing/SrtParser.cs
@@ -0,0 +1,74 @@
+﻿using System.Globalization;
+
+public static class SrtParser
+{
+    public static List<SubtitleRecord> Parse(string path)
+    {
+        var lines = File.ReadAllLines(path);
+
+        var result = new List<SubtitleRecord>();
+
+        int i = 0;
+
+        while (i < lines.Length)
+        {
+            // skip lege regels
+            while (i < lines.Length && string.IsNullOrWhiteSpace(lines[i]))
+            {
+                i++;
+            }
+
+            if (i >= lines.Length)
+            {
+                break;
+            }
+
+            // index
+            var index = int.Parse(lines[i]);
+            i++;
+
+            // timing
+            var timing = lines[i];
+            i++;
+
+            var split = timing.Split(" --> ");
+
+            var start = ParseTime(split[0]);
+            var end = ParseTime(split[1]);
+
+            // text
+            var textLines = new List<string>();
+
+            while (i < lines.Length && !string.IsNullOrWhiteSpace(lines[i]))
+            {
+                textLines.Add(lines[i]);
+                i++;
+            }
+
+            result.Add(new SubtitleRecord(
+                index,
+                start,
+                end,
+                textLines));
+        }
+
+        return result;
+    }
+
+    private static TimeSpan ParseTime(string value)
+    {
+        return TimeSpan.ParseExact(
+            value,
+            @"hh\:mm\:ss\,fff",
+            CultureInfo.InvariantCulture);
+    }
+}
+
+public record SubtitleRecord(
+    int Index,
+    TimeSpan Start,
+    TimeSpan End,
+    List<string> Lines)
+{
+    public string Text => string.Join(Environment.NewLine, Lines);
+}
